9013e507fb76f88de728b829b72972618ce9af9c
Sven Walter <sven@wltr.eu>
2016-05-13 20:03:52 +0200

    Initialer Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# sven.wltr.xyz
+
+* http://sven.wltr.xyz
+
+Personal web page using [Google Cloud Storage](https://cloud.google.com/storage/docs/hosting-static-website).
---
+++ hack/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -xe
+
+gsutil \
+	rsync \
+	-d -r \
+	public \
+	gs://sven.wltr.xyz
+
+gsutil \
+	acl ch \
+	-r \
+	-u AllUsers:R \
+	gs://sven.wltr.xyz
